0e680c349c5bebb194ef69a62856402a5996f3d8
balazsnorbi <balazsnorbi@gmail.com>
2014-10-27 21:31:31 +0200

    Initial commit.

---
+++ Celule/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * @since Oct 27, 2014
+ */
+
+/**
+ * @author Norbi
+ * @since Oct 27, 2014 9:30:10 PM
+ */
+public class Main {
+
+	/**
+	 * @author Norbi
+	 * @since Oct 27, 2014 9:30:15 PM
+	 * @brief Constructor
+	 */
+	public Main() {
+		System.out.println("Hello all!");
+	}
+}
